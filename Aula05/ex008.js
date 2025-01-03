b59ed6d95ae9196c797009e952e16416cec253ed
Leonardo Fogaça <leonardogfogaca07@gmail.com>
2025-01-02 22:29:56 -0300

    aulas

---
+++ Aula05/ex008.js
@@ -0,0 +1,10 @@
+var vel = 80
+console.log(`A velocidade do carro é de ${vel}Km/h!`)
+if (80>vel>60){//CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou os limites de velocidade. Multado!`)
+}
+if(vel>=80){
+    console.log(`Você está muito acima dos limites de velocidade. Sua carteira está suspensa`)
+}
+console.log(`Dirija sempre com segurança`)
+
---
+++ Aula05/ex009.js
@@ -0,0 +1,8 @@
+var country = 'Brasil'
+console.log(`Nascido no ${country}`)
+if(country == 'Brasil'){
+    console.log('Você é Brasileiro')
+}
+else{
+    console.log('Você é estrangeiro')
+}
